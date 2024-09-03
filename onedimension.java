510a7e0c1c334c08bd99cd6b5503468dfcf8cb30
rajlakshman <rajlakshman138@gmail.com>
2024-09-03 17:03:35 +0530

    Add files via upload

---
+++ onedimension.java
@@ -0,0 +1,12 @@
+public class onedimension
+{
+public static void main(String[]args)
+{
+System.out.println("today topic one=dimensionarray");
+int[]arr={90,80,70,60,50};
+int n=arr.length;
+for(int i=0;i<n;i++){
+System.out.println(arr[i]);
+} 
+}
+}
